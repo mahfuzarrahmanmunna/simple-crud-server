985b1703e9422515cfe33dcf3ac0b12df560f5c5
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-14 02:35:08 +0600

    added client post and add to fetch data

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('userDB').collection('users')
+    
+    // Get Method
+    app.get("/users", async(req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // Create Post method
+    app.post('/users', async(req, res) => {
+      const newUsers = req.body;
+      const result = await userCollection.insertOne(newUsers);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -35,7 +53,8 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Crud server is running')
+  res.send('Crud server is running');
+
 })
 
 
